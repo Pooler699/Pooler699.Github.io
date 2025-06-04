464078680ad535f970ebb3a0d4c8778f65c7f755
Pooler699 <pooler69951@students.apsb.org>
2025-06-04 16:19:38 +0000

    finished

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,14 +20,28 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
+        var circle;
+        var circles = [];
+
+
 
 
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+physikz.addRandomVelocity(circle, canvas, 5, 5);
+view.addChild(circle);
+circles.push(circle);
+        }
 
 
         // TODO 3 : Call the drawCircle() function
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
 
 
 
@@ -47,10 +61,23 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
+             physikz.updatePosition(circles[0])
+             physikz.updatePosition(circles[1])
+             physikz.updatePosition(circles[2])
+             physikz.updatePosition(circles[3])
+             physikz.updatePosition(circles[4])
+             
+        
+
 
             
             // TODO 5 : Call game.checkCirclePosition() on your circles
-           
+             Call game.checkCirclePosition(0)
+             Call game.checkCirclePosition(1)
+             Call game.checkCirclePosition(2)
+             Call game.checkCirclePosition(3)
+             Call game.checkCirclePosition(4)
+
 
             // TODO 8 / TODO 9 : Iterate over the array
            
